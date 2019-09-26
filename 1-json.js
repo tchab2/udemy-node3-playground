ddd5e55b26eba55cded8ee43bed1d8b1b04cb9a3
Chabot-Beaulieu <sylvain.chabot-beaulieu@cgi.com>
2019-09-26 11:10:09 -0400

    Remove commented code

--- 1-json.js
+++ 1-json.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
 
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Jhon'
-// }
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Jhon'
+}
 
-// const bookJSON = JSON.stringify(book);
-// console.log(bookJSON);
+const bookJSON = JSON.stringify(book);
+console.log(bookJSON);
 
-// const bookData = JSON.parse(bookJSON);
-// console.log(bookData.author);
+const bookData = JSON.parse(bookJSON);
+console.log(bookData.author);
 
-// fs.writeFileSync('1-json.json', bookJSON);
+fs.writeFileSync('1-json.json', bookJSON);
 
-// const dataBuffer = fs.readFileSync('1-json.json');
-// const dataJSON = dataBuffer.toString();
-// const data = JSON.parse(dataJSON);
-// console.log(data);
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data);
 
-// const dataBuffer = fs.readFileSync('1-json.json');
-// let dataJSON = dataBuffer.toString();
-// const data = JSON.parse(dataJSON);
-// data.name = "Sylvain";
+const dataBuffer = fs.readFileSync('1-json.json');
+let dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+data.name = "Sylvain";
 
-// dataJSON = JSON.stringify(data);
-// fs.writeFileSync('1-json.json', dataJSON);
+dataJSON = JSON.stringify(data);
+fs.writeFileSync('1-json.json', dataJSON);
